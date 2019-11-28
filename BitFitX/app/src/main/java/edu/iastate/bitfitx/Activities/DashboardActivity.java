d0892fdffc13facdd29d901adae66ffebf04b25b
shubham <shubham@iastate.edu>
2019-11-27 19:31:40 -0600

    Dashboard nagivation is complete

--- BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/DashboardActivity.java
+++ BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/DashboardActivity.java
@@ -17,11 +17,46 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.dashboard_layout);
 
 
+        setUpNavigationActivities();
+
+
+    }
+
+    /**
+     * This method sets up all the buttons
+     * and the activity navigation on button click.
+     */
+    private void setUpNavigationActivities() {
+
         Button logWorkoutButton = findViewById(R.id.log_workout);
         logWorkoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(DashboardActivity.this, LogWorkoutActivity.class));
+            }
+        });
+
+        Button trackWorkoutButton = findViewById(R.id.track_workout);
+        trackWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardActivity.this, TrackWorkoutActivity.class));
+            }
+        });
+
+        Button communityUsersButton = findViewById(R.id.community_users);
+        communityUsersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardActivity.this, CommunityListActivity.class));
+            }
+        });
+
+        Button workoutHistoryButton = findViewById(R.id.personal_history);
+        workoutHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardActivity.this, WorkoutHistoryActivity.class));
             }
         });
     }
