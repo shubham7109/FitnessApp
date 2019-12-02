8376b55db4f6b8cf22264548c2fe75f3993e8952
Emily Kinne <emilykinne@emily-macbook.student.iastate.edu>
2019-12-02 15:14:28 -0600

    Updated Login page, it is not logging in a user correctly

--- BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/DashboardActivity.java
+++ BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/DashboardActivity.java
@@ -2,7 +2,9 @@ package edu.iastate.bitfitx.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +13,27 @@ import edu.iastate.bitfitx.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    /**
+     * Shared Preferences used to automatically save the user's email
+     */
+    private SharedPreferences mSharedPreferences;
+    private String username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_layout);
 
+        mSharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        mSharedPreferences.edit().clear().commit();
+
+        if (mSharedPreferences.getString("email", null)==null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        else{
+            username = mSharedPreferences.getString("email", null);
+        }
 
         Button logWorkoutButton = findViewById(R.id.log_workout);
         logWorkoutButton.setOnClickListener(new View.OnClickListener() {
--- BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/LoginActivity.java
+++ BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/LoginActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.content.SharedPreferences;
 
@@ -18,29 +18,29 @@ import org.w3c.dom.Text;
 import edu.iastate.bitfitx.Models.UserModel;
 
 import edu.iastate.bitfitx.R;
+import edu.iastate.bitfitx.Utils.DataProvider;
+import edu.iastate.bitfitx.Utils.Interfaces;
 
 public class LoginActivity extends AppCompatActivity {
 
     /**
-     * Element for entering the email
-     */
-    private TextView email;
-    /**
-     * Element for entering the password
-     */
-    private TextView password;
-    /**
      * String to store the user's email
      */
-    private String username;
+     String email;
     /**
-     * Shared Preferences used to automatically save the user's email
+     * String to store the user's password
      */
-    private SharedPreferences mSharedPreferences;
+     String password;
+
+     SharedPreferences mSharedPreferences;
     /**
      * Title of the list that is going to be edited. The string is passed to the second activity for editing.
      */
     public static final String USER = "";
+
+    DataProvider dp;
+    TextView errorMsg;
+    //EditText password_txt;
 
     //True, when we need to run tests for the database
     private final static boolean IS_DATABASE_TEST_ENABLED = false;
@@ -50,54 +50,42 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        dp = DataProvider.getInstance();
+
         mSharedPreferences = getPreferences(Context.MODE_PRIVATE);
-        if (mSharedPreferences.getString("email", null)!=null){
-            username = mSharedPreferences.getString("email", null);
-            Intent intent = new Intent(this, DashboardActivity.class);
-            intent.putExtra(USER, username);
-            startActivity(intent);
-        }
+
         //DO NOT REMOVE THIS IF STATEMENT
         if (IS_DATABASE_TEST_ENABLED) {
             startActivity(new Intent(this, DatabaseTestActivity.class));
         }
 
-        //TODO Add stuff here.. Maybe login/registration page.
-        email = findViewById(R.id.email_text);
-        password = findViewById(R.id.password_text);
+        final EditText email_txt = (EditText) findViewById(R.id.email_text);
+        final EditText password_txt = (EditText) findViewById(R.id.password_text);
+        errorMsg = (TextView) findViewById(R.id.error_txt) ;
+        errorMsg.setVisibility(View.INVISIBLE);
 
-        email.addTextChangedListener(new TextWatcher() {
+        Button login = (Button) findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
+            public void onClick(View view) {
+                email = email_txt.getText().toString();
+                password = password_txt.getText().toString();
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                try {
-                    username = (s.toString());
-                } catch (Exception e) {
-                    username = "";
-                }
-            }
+                dp.getUser(email, new Interfaces.UserCallback() {
+                    @Override
+                    public void onCompleted(UserModel user) {
+                        openDashboard(email);
+                    }
 
-            @Override
-            public void afterTextChanged(Editable s) {
+                    @Override
+                    public void onError(String msg) {
+                        email_txt.getText().clear();
+                        password_txt.getText().clear();
+                        errorMsg.setVisibility(View.VISIBLE);
+                    }
+                });
             }
-
         });
-    }
-
-    /**
-     * Saves the username and starts dashboard activity
-     * @param view Login button that was clicked
-     */
-    public void onLoginClicked (View view) {
-        SharedPreferences.Editor editor = mSharedPreferences.edit();
-        editor.putString("email", username);
-        editor.commit();
-        Intent intent = new Intent(this, DashboardActivity.class);
-        intent.putExtra(USER, username);
-        startActivity(intent);
     }
 
     /**
@@ -106,6 +94,16 @@ public class LoginActivity extends AppCompatActivity {
      */
     public void onRegisterClicked (View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Method to open the Dashboard activity when login is successful. It will pass the user's email to the next activity.
+     * @param String The user's email
+     */
+    public void openDashboard(String email){
+        Intent intent = new Intent(this, DashboardActivity.class);
+        intent.putExtra(USER, email);
         startActivity(intent);
     }
 
