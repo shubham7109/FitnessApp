a4ff040d776665a5b52ba99cba005c22d77f4b5d
shubham <shubham@iastate.edu>
2019-12-11 00:42:19 -0600

    Added weight history list rv

--- BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/WeightGraphActivity.java
+++ BitFitX/app/src/main/java/edu/iastate/bitfitx/Activities/WeightGraphActivity.java
@@ -1,11 +1,18 @@
 package edu.iastate.bitfitx.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
@@ -18,10 +25,13 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.OnDataPointTapListener;
 import com.jjoe64.graphview.series.Series;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 import edu.iastate.bitfitx.Models.WeightModel;
 import edu.iastate.bitfitx.R;
@@ -29,6 +39,8 @@ import edu.iastate.bitfitx.Utils.DataProvider;
 import edu.iastate.bitfitx.Utils.Interfaces;
 
 public class WeightGraphActivity extends AppCompatActivity {
+
+    ArrayList<WeightModel> weightModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +57,12 @@ public class WeightGraphActivity extends AppCompatActivity {
         }
 
 
+
         DataProvider dataProvider = DataProvider.getInstance();
         dataProvider.getUsersWeight(email, new Interfaces.WeightListCallback() {
             @Override
             public void onCompleted(ArrayList<WeightModel> weightModels) {
+                WeightGraphActivity.this.weightModels = new ArrayList<>(weightModels);
                 Collections.reverse(weightModels);
                 setUpGraph(weightModels);
                 setUpRV();
@@ -62,6 +76,10 @@ public class WeightGraphActivity extends AppCompatActivity {
     }
 
     private void setUpRV() {
+        final RecyclerView recyclerView = findViewById(R.id.weight_history_rv);
+        WeightHistoryAdapter adapter = new WeightHistoryAdapter(WeightGraphActivity.this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(WeightGraphActivity.this));
+        recyclerView.setAdapter(adapter);
     }
 
     private void setUpGraph(ArrayList<WeightModel> weightModels) {
@@ -102,5 +120,55 @@ public class WeightGraphActivity extends AppCompatActivity {
         graph.getViewport().setMinX(new Date(weightModels.get(0).getTimeInMillis()).getTime());
         graph.getViewport().setMaxX(new Date(weightModels.get(weightModels.size()-1).getTimeInMillis()).getTime());
         graph.getViewport().setXAxisBoundsManual(true);
+    }
+
+
+    public class WeightHistoryAdapter extends RecyclerView.Adapter<WeightHistoryAdapter.ViewHolder> {
+
+        private LayoutInflater mInflater;
+
+        WeightHistoryAdapter(Context context) {
+            this.mInflater = LayoutInflater.from(context);
+        }
+
+        // inflates the row layout from xml when needed
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = mInflater.inflate(R.layout.weight_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        // binds the data to the TextView in each row
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            WeightModel weightModel = weightModels.get(position);
+            Date date = new Date(weightModel.getTimeInMillis());
+            DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            format.setTimeZone(TimeZone.getDefault());
+
+            holder.weight.setText(weightModel.getWeightInPounds() + "lbs");
+            holder.date.setText(format.format(date));
+
+        }
+
+        // total number of rows
+        @Override
+        public int getItemCount() {
+            return weightModels.size();
+        }
+
+
+        // stores and recycles views as they are scrolled off screen
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            TextView date;
+            TextView weight;
+
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                date = itemView.findViewById(R.id.date);
+                weight = itemView.findViewById(R.id.weight);
+            }
+        }
     }
 }
